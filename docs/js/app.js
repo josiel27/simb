5c64902773ab98decdb718df79a7f4e460d135f9
Josiel da Sillva Faria <josiel.faria@grupont.com.br>
2018-03-01 17:02:04 -0300

    subindo fullcalendar

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,13 +1,27 @@
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function ($routeProvider) {
     $routeProvider
-        .when("/", {
-            templateUrl: "main.htm"
-        })
-        .when("/home", {
+        .when("/!", {
             templateUrl: "home.html"
         })
         .when("/usuarios", {
-            templateUrl: "usuarios.html"
+            templateUrl: "usuarios.html",
+            function: consultarUsuarios
+        })
+        .when("/alunos", {
+            templateUrl: "alunos.html",
+            function: consultarAlunos()
+        })
+        .when("/calendario", {
+            templateUrl: "calendario.html"
         });
 });
+
+function consultarUsuarios(){
+    console.log('Consultar Usuarios')
+}
+
+
+function consultarAlunos(){
+    console.log('Consultar Alunos')
+}
